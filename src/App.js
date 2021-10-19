2624c879ece76cc75bdde6df08c08d590c1e8e36
jcccookie <doublekn88@gmail.com>
2021-10-19 00:26:02 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GoogleButton from "react-google-button";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
+  const handleGoogleLoginButton = () => {
+    window.location.assign(
+      // "https://quiz-user-329402.uc.r.appspot.com/auth/google"
+      "http://localhost:8080/auth/google"
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is Home
+        <Link to="/login">Login Page</Link>
+      </Route>
+      <Route exact path="/login">
+        <GoogleButton onClick={handleGoogleLoginButton} />
+      </Route>
+      <Route exact path="/dashboard">
+        This is dashboard
+      </Route>
+    </Switch>
   );
 }
 
