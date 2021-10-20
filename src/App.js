7b7acf11b527fcb63d26b5613e5e5d8e36d738bb
jcccookie <doublekn88@gmail.com>
2021-10-20 08:36:13 -0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import axios from "axios";
 import GoogleButton from "react-google-button";
 import { Link, Route, Switch } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 function App() {
   const handleGoogleLoginButton = () => {
@@ -10,17 +12,23 @@ function App() {
     );
   };
 
+  const handleLogout = () => {
+    axios.get("http://localhost:8080/logout");
+  };
+
   return (
     <Switch>
-      <Route exact path="/">
-        This is Home
-        <Link to="/login">Login Page</Link>
-      </Route>
+      {["/", "/home", "/dashboard"].map((path) => (
+        <Route key={path} exact path={path}>
+          <Link to="/login">Login Page</Link>
+          <Button onClick={handleLogout}>Logout</Button>
+        </Route>
+      ))}
       <Route exact path="/login">
         <GoogleButton onClick={handleGoogleLoginButton} />
       </Route>
-      <Route exact path="/dashboard">
-        This is dashboard
+      <Route exact path="/login/error">
+        Login Error!
       </Route>
     </Switch>
   );
