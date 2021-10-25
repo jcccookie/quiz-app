20f57cca5b4d2e31a1ca2a30f788811bf5c8fc5d
jcccookie <doublekn88@gmail.com>
2021-10-24 21:44:31 -0700

    Add Nav bar and Dashboard components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 import axios from "axios";
-import GoogleButton from "react-google-button";
-import { Link, Route, Switch } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  const handleGoogleLoginButton = () => {
-    window.location.assign(
-      // "https://quiz-user-329402.uc.r.appspot.com/auth/google"
-      "http://localhost:8080/auth/google"
-    );
-  };
-
   const handleLogoutButton = () => {
     axios
       .get("http://localhost:8080/logout")
@@ -24,22 +17,19 @@ function App() {
   };
 
   return (
-    <Switch>
-      {["/", "/home", "/dashboard"].map((path) => (
-        <Route key={path} exact path={path}>
-          <Link to="/login">Login Page</Link>
-          <Button variant="primary" onClick={handleLogoutButton}>
-            Logout
-          </Button>
+    <>
+      <Navigation />
+      <Switch>
+        {["/", "/home", "/dashboard"].map((path) => (
+          <Route key={path} exact path={path}>
+            <Dashboard />
+          </Route>
+        ))}
+        <Route exact path="/login/error">
+          Login Error!
         </Route>
-      ))}
-      <Route exact path="/login">
-        <GoogleButton onClick={handleGoogleLoginButton} />
-      </Route>
-      <Route exact path="/login/error">
-        Login Error!
-      </Route>
-    </Switch>
+      </Switch>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
