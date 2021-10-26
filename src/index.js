4f543fdd5bef31e2652949ead38a3aa9a73734cc
jcccookie <doublekn88@gmail.com>
2021-10-25 22:53:50 -0700

    Minor changes

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
