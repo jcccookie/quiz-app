99900ef44fa83bb654aa9a002644fdd26e73a44a
brynsroberts <bryan.roberts.home@gmail.com>
2021-10-28 17:20:23 -0700

    add spinner for loading NewQuestion components and add event handlers for clicking new Question buttons

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -9,8 +9,24 @@ import {
 } from "react-bootstrap";
 
 function NewQuestion(props) {
+  // new True False Question
   function newTrueFalseQuestion() {
     console.log("new true false question");
+  }
+
+  // new multiple choice question
+  function newMultipleChoiceQuestion() {
+    console.log("new multiple choice question");
+  }
+
+  // new check all that apply question
+  function newCheckAllThatApplyQuestion() {
+    console.log("new check all that apply question");
+  }
+
+  // new free form question
+  function newFreeFormQuestion() {
+    console.log("new free form question");
   }
 
   return (
@@ -32,11 +48,21 @@ function NewQuestion(props) {
                     <Dropdown.Item eventKey="1" onClick={newTrueFalseQuestion}>
                       True/False
                     </Dropdown.Item>
-                    <Dropdown.Item eventKey="2">Multiple Choice</Dropdown.Item>
-                    <Dropdown.Item eventKey="3">
+                    <Dropdown.Item
+                      eventKey="2"
+                      onClick={newMultipleChoiceQuestion}
+                    >
+                      Multiple Choice
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      eventKey="3"
+                      onClick={newCheckAllThatApplyQuestion}
+                    >
                       Check All That Apply
                     </Dropdown.Item>
-                    <Dropdown.Item eventKey="4">Free Form</Dropdown.Item>
+                    <Dropdown.Item eventKey="4" onClick={newFreeFormQuestion}>
+                      Free Form
+                    </Dropdown.Item>
                   </DropdownButton>
                 </Col>
                 <Col className="text-center">
