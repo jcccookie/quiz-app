4682afdb5834c13c595c1950086f7d17b3dd0abb
brynsroberts <bryan.roberts.home@gmail.com>
2021-10-29 17:30:24 -0700

    add cancel button to truefalsequestion form

--- src/components/TrueFalseQuestion.js
+++ src/components/TrueFalseQuestion.js
@@ -1,10 +1,4 @@
-import {
-  Container,
-  Col,
-  Row,
-  Form,
-  Button,
-} from "react-bootstrap";
+import { Container, Col, Row, Form, Button, Card } from "react-bootstrap";
 
 function TrueFalseQuestion(props) {
   const formSubmissionHandler = async (event) => {
@@ -13,31 +7,58 @@ function TrueFalseQuestion(props) {
     props.setQuestionType(0);
   };
 
+  const onClickCancel = (event) => {
+    event.preventDefault();
+    props.setQuestionType(0);
+  };
+
   return (
     <Container>
       <Row>
         <Col>
-          <Form onSubmit={formSubmissionHandler}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Question</Form.Label>
-              <Form.Control type="text" placeholder="Enter question" />
-            </Form.Group>
-            <Form.Check
-              type="radio"
-              label="true"
-              name="formHorizontalRadios"
-              id="true"
-            />
-            <Form.Check
-              type="radio"
-              label="false"
-              name="formHorizontalRadios"
-              id="false"
-            />
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+          <Card>
+            <Card.Header>New True/False Question</Card.Header>
+            <Card.Body>
+              <Form onSubmit={formSubmissionHandler}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Question</Form.Label>
+                  <Form.Control type="text" placeholder="Enter question" />
+                </Form.Group>
+                <Form.Check
+                  type="radio"
+                  label="True"
+                  name="formHorizontalRadios"
+                  id="true"
+                />
+                <Form.Check
+                  type="radio"
+                  label="False"
+                  name="formHorizontalRadios"
+                  id="false"
+                />
+                <Row>
+                  <Col className="text-center">
+                    <Button
+                      className="text-center"
+                      variant="primary"
+                      type="submit"
+                    >
+                      Submit
+                    </Button>
+                  </Col>
+                  <Col className="text-center">
+                    <Button
+                      variant="warning"
+                      type="submit"
+                      onClick={onClickCancel}
+                    >
+                      Cancel
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
